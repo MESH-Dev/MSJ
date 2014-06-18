ef3f079f2b67a68f05328f1d092aa7845c4eb9ee
Pat Eason <patrick@pateason.com>
2014-06-17 22:19:54 -0400

    working on front-page post entry styling

--- front-page.php
+++ front-page.php
@@ -1,22 +1,21 @@
 <?php get_header(); ?>
 
 <section id="content">
-  <div class="container">
-    <div class="gutter">
+  <div class="container cf">
+    <div class="gutter cf">
 
       <section id="contentPrimary">
-        <div class="gutter">
-          <?php the_content(); ?>
-        </div>
-      </section>
-
-      <section id="contentSecondary">
         <?php if(have_posts()){while(have_posts()){the_post(); ?>
           <article class="post-entry">
             <div class="gutter">
               <div class="post-entry-date">
-                <i class="fa fa-calendar-o"></i> <?php the_date(); ?>
+                <i class="fa fa-calendar-o"></i> <?php $dFormat = get_option('date_format'); the_time($dFormat); ?>
               </div>
+              <?php if(has_post_thumbnail()){ ?>
+                <div class="post-entry-feat">
+                  <?php the_post_thumbnail(); ?>
+                </div>
+              <?php } ?>
               <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
               <div class="post-excerpt">
                 <?php the_excerpt(); ?>
@@ -28,16 +27,26 @@
                 <div class="post-meta-comments">
                   <i class="fa fa-comment-o"></i> <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
                 </div>
-                <div class="post-meta-category">
-                  <i class="fa fa-folder-o"></i> <?php the_category(); ?>
-                </div>
-                <div class="post-meta-tags">
-                  <i class="fa fa-tags"></i> <?php the_tags(''); ?>
-                </div>
+                <?php if(has_category()){ ?>
+                  <div class="post-meta-category">
+                    <i class="fa fa-folder-o"></i> <?php the_category(', '); ?>
+                  </div>
+                <?php } ?>
+                <?php if(has_tag()){ ?>
+                  <div class="post-meta-tags">
+                    <i class="fa fa-tags"></i> <?php the_tags(''); ?>
+                  </div>
+                <?php } ?>
               </div>
             </div>
           </article>
         <?php } } ?>
+      </section>
+
+      <section id="contentSecondary">
+        <div class="gutter">
+          <?php dynamic_sidebar('blog-sidebar'); ?>
+        </div>
       </section>
 
     </div>
