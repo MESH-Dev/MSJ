171fa15f3b8d4ee5b6d90e6fcac04b08db79e661
Pat Eason <patrick@pateason.com>
2014-07-17 15:41:12 -0400

    full WP w/ deploy

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<?php if(have_posts()){while(have_posts()){the_post(); ?>
-	<div class="post-entry">
-    <div class="gutter">
-      <h3><?php the_title(); ?></h3>
-      <div class="post-meta">
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-      </div>
-      <div class="post-excerpt">
-        <?php the_excerpt(); ?>
-      </div>
-    </div>
-  </div>
-<?php } } ?>
-
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
